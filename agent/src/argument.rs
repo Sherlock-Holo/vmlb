ed8fb997be28e6aefdf3b9cd0805c421ec7f3435
Sherlock Holo <sherlockya@gmail.com>
2022-09-02 23:09:45 +0800

    docs(agent): add doc for argument

--- agent/src/argument.rs
+++ agent/src/argument.rs
@@ -8,11 +8,14 @@ use clap::Parser;
 #[clap(version, about)]
 pub struct Argument {
     #[clap(short, long)]
+    /// bridge nic name
     pub bridge: String,
 
     #[clap(short, long, default_value_os_t = PathBuf::from("/run/vmlb"))]
+    /// record persistent dir
     pub persistent_dir: PathBuf,
 
     #[clap(short, long, default_value_t = SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::from([0, 0, 0, 0]), 8599)))]
+    /// grpc service listen addr
     pub listen_addr: SocketAddr,
 }
