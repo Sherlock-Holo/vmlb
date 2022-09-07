b9a4f9e07947a4471e953cc1831b1408e365fa28
Sherlock Holo <sherlockya@gmail.com>
2022-09-07 22:37:29 +0800

    feat(agent): check dhclient dead

--- agent/src/addr_allocate/veth.rs
+++ agent/src/addr_allocate/veth.rs
@@ -1,9 +1,13 @@
 use std::io::{Error, ErrorKind};
 use std::net::Ipv4Addr;
+use std::os::unix::ffi::OsStrExt;
 use std::process::Stdio;
+use std::sync::Arc;
+use std::time::Duration;
 
+use dashmap::DashSet;
 use futures_util::future::AbortHandle;
-use futures_util::{future, TryStreamExt};
+use futures_util::{future, StreamExt, TryStreamExt};
 use md5::{Digest, Md5};
 use nix::errno::Errno;
 use rtnetlink::packet::address::Nla as AddressNla;
@@ -13,6 +17,8 @@ use rtnetlink::Handle;
 use tap::TapFallible;
 use tokio::io::AsyncReadExt;
 use tokio::process::Command;
+use tokio::{fs, time};
+use tokio_stream::wrappers::IntervalStream;
 use tracing::{error, info, warn};
 
 use crate::addr_allocate::AddrAllocate;
@@ -20,9 +26,14 @@ use crate::addr_allocate::AddrAllocate;
 const MAX_NIC_NAME_LEN: usize = 13;
 
 pub struct VethAddrAllocate {
+    inner: Arc<VethAddrAllocateInner>,
+}
+
+struct VethAddrAllocateInner {
     bridge_index: u32,
     handle: Handle,
     netlink_task_stop: AbortHandle,
+    nic_list: DashSet<String>,
 }
 
 impl VethAddrAllocate {
@@ -49,15 +60,25 @@ impl VethAddrAllocate {
                 )
             })?;
 
-        Ok(Self {
+        let inner = Arc::new(VethAddrAllocateInner {
             bridge_index: link_message.header.index,
             handle,
             netlink_task_stop: abort_handle,
-        })
+            nic_list: Default::default(),
+        });
+
+        {
+            // for now we don't want the VethAddrAllocate cloneable yet
+            let inner = inner.clone();
+            tokio::spawn(async move { Self { inner }.check_dhclient_alive().await });
+        }
+
+        Ok(Self { inner })
     }
 
     async fn allocate_veth(&self, nic_name: &str, peer: &str) -> Result<(), Error> {
-        self.handle
+        self.inner
+            .handle
             .link()
             .add()
             .veth(nic_name.to_string(), peer.to_string())
@@ -72,6 +93,7 @@ impl VethAddrAllocate {
         info!(nic_name, peer, "add veth done");
 
         let peer_info = self
+            .inner
             .handle
             .link()
             .get()
@@ -100,10 +122,11 @@ impl VethAddrAllocate {
             "get peer index done"
         );
 
-        self.handle
+        self.inner
+            .handle
             .link()
             .set(peer_info.header.index)
-            .master(self.bridge_index)
+            .master(self.inner.bridge_index)
             .execute()
             .await
             .map_err(|err| {
@@ -115,7 +138,7 @@ impl VethAddrAllocate {
         info!(
             nic_name,
             peer,
-            bridge_index = self.bridge_index,
+            bridge_index = self.inner.bridge_index,
             "set peer master to bridge done"
         );
 
@@ -123,7 +146,10 @@ impl VethAddrAllocate {
     }
 
     async fn allocate_ip(&self, nic_name: &str) -> Result<Vec<String>, Error> {
+        let pid_file = format!("/run/dhclient.{}.pid", nic_name);
+
         let mut child = Command::new("dhclient")
+            .args(["-pf", &pid_file])
             .arg(nic_name)
             .spawn()
             .tap_err(|err| error!(%err, nic_name, "spawn dhclient failed"))?;
@@ -175,7 +201,13 @@ impl VethAddrAllocate {
             Error::new(ErrorKind::NotFound, format!("nic {} not exists", nic_name))
         })?;
 
-        self.get_exists_veth_ip(nic_name, &link_message).await
+        let veth_ip = self.get_exists_veth_ip(nic_name, &link_message).await?;
+
+        info!(nic_name, ?veth_ip, "get veth ip done");
+
+        self.inner.nic_list.insert(nic_name.to_string());
+
+        Ok(veth_ip)
     }
 
     async fn nic_is_veth(&self, nic_name: &str, link_message: &LinkMessage) -> Result<bool, Error> {
@@ -213,6 +245,7 @@ impl VethAddrAllocate {
         link_message: &LinkMessage,
     ) -> Result<Vec<String>, Error> {
         let addresses = self
+            .inner
             .handle
             .address()
             .get()
@@ -250,6 +283,7 @@ impl VethAddrAllocate {
 
     async fn get_nic_info(&self, nic_name: &str) -> Result<Option<LinkMessage>, Error> {
         match self
+            .inner
             .handle
             .link()
             .get()
@@ -271,6 +305,143 @@ impl VethAddrAllocate {
             }
 
             Ok(link) => Ok(link),
+        }
+    }
+
+    async fn deallocate_ip(&self, nic_name: &str) -> Result<(), Error> {
+        let mut child = Command::new("dhclient")
+            .args(["-r", nic_name])
+            .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
+            .spawn()
+            .tap_err(|err| error!(%err, nic_name, "spawn dhclient -r failed"))?;
+        let exit_status = child
+            .wait()
+            .await
+            .tap_err(|err| error!(%err, nic_name, "wait dhclient -r process failed"))?;
+
+        if !exit_status.success() {
+            if let Some(exit_code) = exit_status.code() {
+                error!(exit_code, nic_name, "dhclient -r failed");
+            } else {
+                error!(nic_name, "dhclient -r failed");
+            }
+
+            let mut stdout = child.stdout.take().unwrap();
+            let mut stdout_buf = vec![];
+            if stdout
+                .read_to_end(&mut stdout_buf)
+                .await
+                .tap_err(|err| error!(%err, nic_name, "read dhclient -r stdout failed"))
+                .is_ok()
+            {
+                error!(nic_name, stdout = ?String::from_utf8_lossy(&stdout_buf), "dhclient -r stdout");
+            }
+
+            let mut stderr = child.stderr.take().unwrap();
+            let mut stderr_buf = vec![];
+            if stderr
+                .read_to_end(&mut stderr_buf)
+                .await
+                .tap_err(|err| error!(%err, nic_name, "read dhclient -r stderr failed"))
+                .is_ok()
+            {
+                error!(nic_name, stderr = ?String::from_utf8_lossy(&stderr_buf), "dhclient -r stdout");
+            }
+        }
+
+        Ok(())
+    }
+
+    async fn check_dhclient_alive(&self) {
+        let mut interval = IntervalStream::new(time::interval(Duration::from_secs(10)));
+
+        while (interval.next().await).is_some() {
+            let mut dead_dhcp_nic_list = vec![];
+
+            for nic_name in self.inner.nic_list.iter() {
+                let nic_name = &*nic_name;
+                let pid_file = format!("/run/dhclient.{}.pid", nic_name);
+
+                let pid = match fs::read(&pid_file).await {
+                    Err(err) if err.kind() == ErrorKind::NotFound => {
+                        warn!(%nic_name, %err, "nic dhclient instance miss");
+
+                        dead_dhcp_nic_list.push(nic_name.clone());
+
+                        continue;
+                    }
+
+                    Err(err) => {
+                        error!(%nic_name, %err, "read nic dhclient pid file failed");
+
+                        dead_dhcp_nic_list.push(nic_name.clone());
+
+                        continue;
+                    }
+
+                    Ok(pid) => pid,
+                };
+                let pid = String::from_utf8_lossy(&pid);
+                let pid = match pid.parse::<u32>() {
+                    Err(err) => {
+                        error!(%nic_name, %pid, %err, "parse pid to u32 failed");
+
+                        dead_dhcp_nic_list.push(nic_name.clone());
+
+                        continue;
+                    }
+
+                    Ok(pid) => pid,
+                };
+
+                info!(nic_name, pid, "get forked dhclient pid done");
+
+                match fs::read_link(format!("/proc/{}/exe", pid)).await {
+                    Err(err) if err.kind() == ErrorKind::NotFound => {
+                        warn!(%nic_name, %err, "nic dhclient instance miss");
+
+                        dead_dhcp_nic_list.push(nic_name.clone());
+                    }
+
+                    Err(err) => {
+                        error!(%nic_name, pid, %err, "try to check dhclient instance failed");
+
+                        dead_dhcp_nic_list.push(nic_name.clone());
+                    }
+
+                    Ok(path) => {
+                        if !path
+                            .as_os_str()
+                            .as_bytes()
+                            .windows("dhclient".chars().count())
+                            .any(|sub_path| sub_path == b"dhclient")
+                        {
+                            error!(%nic_name, pid, "dhclient pid exists, but it is not dhclient instance");
+
+                            dead_dhcp_nic_list.push(nic_name.clone());
+                        }
+                    }
+                }
+            }
+
+            if !dead_dhcp_nic_list.is_empty() {
+                info!(?dead_dhcp_nic_list, "start reallocate ip for dead dhcp nic");
+
+                for nic_name in dead_dhcp_nic_list.iter() {
+                    match self.allocate_ip(nic_name).await {
+                        Err(err) => {
+                            error!(%nic_name, %err, "reallocate ip for dead dhcp nic failed");
+                        }
+
+                        Ok(ip) => {
+                            // dhclient will try to reuse allocated ip, so no need to pass the ip
+                            // out
+                            info!(%nic_name, ?ip, "reallocate ip for dead dhcp nic done");
+                        }
+                    }
+                }
+            }
         }
     }
 }
@@ -326,46 +497,7 @@ impl AddrAllocate for VethAddrAllocate {
     async fn deallocate(&self, namespace: &str, service: &str) -> Result<(), Self::Error> {
         let (nic_name, _) = get_nic_names(namespace, service);
 
-        let mut child = Command::new("dhclient")
-            .args(["-r", nic_name.as_ref()])
-            .stdout(Stdio::piped())
-            .stderr(Stdio::piped())
-            .spawn()
-            .tap_err(|err| error!(%err, nic_name, "spawn dhclient -r failed"))?;
-        let exit_status = child
-            .wait()
-            .await
-            .tap_err(|err| error!(%err, nic_name, "wait dhclient -r process failed"))?;
-
-        if !exit_status.success() {
-            if let Some(exit_code) = exit_status.code() {
-                error!(exit_code, nic_name, "dhclient -r failed");
-            } else {
-                error!(nic_name, "dhclient -r failed");
-            }
-
-            let mut stdout = child.stdout.take().unwrap();
-            let mut stdout_buf = vec![];
-            if stdout
-                .read_to_end(&mut stdout_buf)
-                .await
-                .tap_err(|err| error!(%err, nic_name, "read dhclient -r stdout failed"))
-                .is_ok()
-            {
-                error!(nic_name, stdout = ?String::from_utf8_lossy(&stdout_buf), "dhclient -r stdout");
-            }
-
-            let mut stderr = child.stderr.take().unwrap();
-            let mut stderr_buf = vec![];
-            if stderr
-                .read_to_end(&mut stderr_buf)
-                .await
-                .tap_err(|err| error!(%err, nic_name, "read dhclient -r stderr failed"))
-                .is_ok()
-            {
-                error!(nic_name, stderr = ?String::from_utf8_lossy(&stderr_buf), "dhclient -r stdout");
-            }
-        }
+        self.deallocate_ip(&nic_name).await?;
 
         info!(%nic_name, "deallocate ip on nic done");
 
@@ -379,7 +511,8 @@ impl AddrAllocate for VethAddrAllocate {
             Some(link_message) => link_message,
         };
 
-        self.handle
+        self.inner
+            .handle
             .link()
             .del(link_message.header.index)
             .execute()
@@ -394,7 +527,7 @@ impl AddrAllocate for VethAddrAllocate {
 
 impl Drop for VethAddrAllocate {
     fn drop(&mut self) {
-        self.netlink_task_stop.abort();
+        self.inner.netlink_task_stop.abort();
     }
 }
 
